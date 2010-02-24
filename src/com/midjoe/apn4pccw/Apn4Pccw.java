fbd8c3552b292acc94c0c2930a70bac221797d5e
kcyim <kcyim@localhost>
2010-02-24 12:43:00 +0800

    - Rename the class name to Apn4Pccw

--- src/com/midjoe/apn4pccw/Apn4Pccw.java
+++ src/com/midjoe/apn4pccw/Apn4Pccw.java
@@ -3,7 +3,7 @@ package com.midjoe.apn4pccw;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class apn4pccw extends Activity {
+public class Apn4Pccw extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
